ed73725ffcee83b1b2abdded79e5be897dfc4f4b
Lucas Godoy - aú desviante <lucasgodoyjor@gmail.com>
2025-03-14 07:54:30 -0300

    feat: Add NNumeros class for user input and division logic
    This commit introduces the NNumeros class, which prompts the user for an initial number and subsequent numbers. The program continues until a number is entered that is not evenly divisible by the initial number.

---
+++ src/calculadora/NNumeros.java
@@ -0,0 +1,19 @@
+// Escreva um código onde o usuário informa um número inicial, posteriormente irá informar outros N números, a execução do código irá continuar até que o número informado dividido pelo primeiro número tenha resto diferente de 0 na divisão, números menores que o primeiro número devem ser ignorados.
+package calculadora;
+
+import java.util.Scanner;
+import util.ValidaValorInt;
+
+public class NNumeros {
+    public NNumeros() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int numero01 = ValidaValorInt.validaValorInt(scanner, "Informe um número inicial: ");
+        int numero02;
+        do{
+            numero02 = ValidaValorInt.validaValorInt(scanner, "Informe outro número: ");
+        } while( numero02 % numero01 != 0);
+    }
+}
